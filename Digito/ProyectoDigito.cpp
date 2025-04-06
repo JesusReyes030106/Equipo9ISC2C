aba2179c381b13b8fb40a781c487ae17160841df
JesusReyes030106 <JesusReyesMar@outlook.com>
2025-04-06 16:23:20 -0600

    Realize algunos cambios al archivo

---
+++ Digito/ProyectoDigito.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+/*
+Proyecto digito / ISBN 
+checar la descripcion del proyecto antes de realizarlo 
+al igual que los requerimientos del proyecto*/
+
+int main(){
+
+    cout << "Hola" ;
+
+    return 0;
+}
